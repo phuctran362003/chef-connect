0e948383cc4a6795b49da94d2cbd6c464be50fb6
phuctran362003 <trangiaphuc362003181@gmail.com>
2024-12-19 14:52:15 +0700

    add validations

--- ChefConnect.BackEnd/ChefConnect.Application/DTOs/User/Request/SignUpRequest.cs
+++ ChefConnect.BackEnd/ChefConnect.Application/DTOs/User/Request/SignUpRequest.cs
@@ -1,9 +1,19 @@
-﻿namespace ChefConnect.Application.DTOs.User.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChefConnect.Application.DTOs.User.Request
 {
     public class SignUpRequest
     {
+        [Required(ErrorMessage = "Username is required.")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+            ErrorMessage = "Password must be at least 8 characters long, contain at least one letter, one number, and one special character.")]
         public string Password { get; set; }
     }
 
