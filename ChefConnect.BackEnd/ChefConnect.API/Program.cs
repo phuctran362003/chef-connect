9c2c93bdc020994227c43dd36aa0cc4baff61f51
phuctran362003 <trangiaphuc362003181@gmail.com>
2024-12-20 15:36:19 +0700

    setup Authen & Author

--- ChefConnect.BackEnd/ChefConnect.API/Program.cs
+++ ChefConnect.BackEnd/ChefConnect.API/Program.cs
@@ -1,67 +1,8 @@
 ﻿using ChefConnect.API.Startup;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureServices(builder.Configuration);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddLogging();
-
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-        ValidAudience = builder.Configuration["JwtSettings:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]))
-    };
-});
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\n\nExample: 'Bearer eyJhbGci...'"
-    });
-
-    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-    {
-        {
-            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-            {
-                Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                {
-                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-});
-
-
-builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -76,8 +17,8 @@ app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseAuthentication(); // Frist
+app.UseAuthorization(); // Second
 
 
 app.MigrateDatabases();
--- ChefConnect.BackEnd/ChefConnect.API/Startup/DI.cs
+++ ChefConnect.BackEnd/ChefConnect.API/Startup/DI.cs
@@ -5,7 +5,11 @@ using ChefConnect.Infrastructure;
 using ChefConnect.Infrastructure.Interfaces;
 using ChefConnect.Infrastructure.Repositories;
 using ChefConnect.Infrastructure.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace ChefConnect.API.Startup
 {
@@ -13,6 +17,130 @@ namespace ChefConnect.API.Startup
     {
         public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            #region Setup Authentication JWT
+            // Setup JWT Authentication
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true, // Enable issuer validation
+                        ValidateAudience = true, // Enable audience validation
+                        ValidateLifetime = true, // Enable token lifetime validation
+                        ValidateIssuerSigningKey = true, // Ensure the signing key is valid
+                        ValidIssuer = configuration["JWT:Issuer"], // Issuer from config
+                        ValidAudience = configuration["JWT:Audience"], // Audience from config
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"])), // Secret key
+                        ClockSkew = TimeSpan.Zero // Optional: remove clock skew for precise expiration
+                    };
+                });
+            #endregion
+
+            #region Setup Authorization
+            services.AddAuthorization(options =>
+            {
+                // Admin-only policy
+                options.AddPolicy(
+                    "AdminOnly",
+                    policyBuilder => policyBuilder.RequireAssertion(
+                        context => context.User.HasClaim(
+                            claim => claim.Type == "Role" && claim.Value == "Admin"
+                        )
+                    )
+                );
+
+                // Chef-only policy
+                options.AddPolicy(
+                    "ChefOnly",
+                    policyBuilder => policyBuilder.RequireAssertion(
+                        context => context.User.HasClaim(
+                            claim => claim.Type == "Role" && claim.Value == "Chef"
+                        )
+                    )
+                );
+
+                // Customer-only policy
+                options.AddPolicy(
+                    "CustomerOnly",
+                    policyBuilder => policyBuilder.RequireAssertion(
+                        context => context.User.HasClaim(
+                            claim => claim.Type == "Role" && claim.Value == "Customer"
+                        )
+                    )
+                );
+
+                // Admin or Chef policy
+                options.AddPolicy(
+                    "AdminOrChef",
+                    policyBuilder => policyBuilder.RequireAssertion(
+                        context => context.User.HasClaim(
+                            claim => claim.Type == "Role" &&
+                            (claim.Value == "Admin" || claim.Value == "Chef")
+                        )
+                    )
+                );
+
+                // Admin, Chef, or Customer policy
+                options.AddPolicy(
+                    "AdminChefOrCustomer",
+                    policyBuilder => policyBuilder.RequireAssertion(
+                        context => context.User.HasClaim(
+                            claim => claim.Type == "Role" &&
+                            (claim.Value == "Admin" || claim.Value == "Chef" || claim.Value == "Customer")
+                        )
+                    )
+                );
+            });
+            #endregion
+
+            #region Setup Swagger Bearer
+
+            services.AddSwaggerGen(c =>
+            {
+                // Define the security scheme
+                var jwtSecurityScheme = new OpenApiSecurityScheme
+                {
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Description = "JWT Authorization header using the Bearer scheme.",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", jwtSecurityScheme);
+
+                // Define the security requirement
+                var securityRequirement = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            };
+
+                c.AddSecurityRequirement(securityRequirement);
+            });
+
+            #endregion
 
             #region UnitOfWork
             services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -62,6 +190,10 @@ namespace ChefConnect.API.Startup
             #region Services
 
             // Đăng ký các service khác nếu có
+            services.AddLogging();
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IPasswordHasher, PasswordHasher>();
